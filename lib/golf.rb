30fe33802ef4d52ac1052063766370697c5c8746
Michael Pavling <pavling@gmail.com>
2014-05-06 19:29:21 +0100

    complete example

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,5 +1,13 @@
 class Golf
   
-
+  def self.hole1(v)
+    (1..v).map { |i|
+      s = ""
+      s << "pling" if i % 3 < 1
+      s << "plang" if i % 5 < 1
+      s << "plong" if i % 7 < 1
+      s[0] ? s : i
+    }
+  end
   
 end
